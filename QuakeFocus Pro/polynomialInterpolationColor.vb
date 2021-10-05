bfc4eda02f5eeb9c20f5967d09d9e347b6617ad0
Wriar <81789627+Wriar@users.noreply.github.com>
2021-10-05 07:39:51 -0600

    Create polynomialInterpolationColor.vb

---
+++ QuakeFocus Pro/polynomialInterpolationColor.vb
@@ -0,0 +1,2 @@
+' TBD: Self-Interpolate Colors using 6th order polynomials. Release requirement for KyoshinMonitorLib and attached 10+ DLLs. 
+' TBD: 6次多項式を使って色を自己補間する。KyoshinMonitorLibと付属の10以上のリファレンスのリリース要求。
